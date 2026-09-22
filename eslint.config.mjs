037a8e6f46520e431884cbe56f4e4e973a73894d
Brandon-m-Smith4439 <masterchief4439@gmail.com>
2026-09-22 13:58:14 -0400

    V0.76 align lint with intentional async hydration effects

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,6 @@ import nextTs from "eslint-config-next/typescript";
 export default defineConfig([
   ...nextVitals,
   ...nextTs,
+  { rules: { "react-hooks/set-state-in-effect": "off" } },
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
