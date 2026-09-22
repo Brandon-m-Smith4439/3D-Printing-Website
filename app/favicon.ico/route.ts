b65f11cb882942e40f5a7c0cfe8bd45c9b050cb5
Brandon-m-Smith4439 <masterchief4439@gmail.com>
2026-09-22 16:51:28 -0400

    Use Next config for favicon fallback

--- app/favicon.ico/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export function GET(request: NextRequest) {
-  const target = request.nextUrl.clone();
-  target.pathname = "/favicon.png";
-  target.search = "";
-  return NextResponse.redirect(target, 308);
-}
